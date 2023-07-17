3708292f3e08c09ae15a6a1c9d8fb0ed11cae799
maksus517 <maksim517@mail.ru>
2023-07-17 21:32:07 +0300

    add admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from users.models import User
+from products.admin import BasketAdmin
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name')
+    inlines = (BasketAdmin,)
