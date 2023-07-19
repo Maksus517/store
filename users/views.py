b9d3b768ea2b958fec1af02c690a27ec2e120b6e
maksus517 <maksim517@mail.ru>
2023-07-19 13:28:41 +0300

    added email verification

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.urls import reverse_lazy
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
@@ -6,7 +8,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from common.views import CommonMixin
 from products.models import Basket
-from users.models import User
+from users.models import User, EmailVerification
 
 
 class UserLoginView(LoginView):
@@ -36,3 +38,18 @@ class UserProfileView(CommonMixin, UpdateView):
         context = super(UserProfileView, self).get_context_data()
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
+
+
+class EmailVerificationView(CommonMixin, TemplateView):
+    title = 'Подтверждение электронной почты'
+    template_name = 'users/email_verification.html'
+
+    def get(self, request, *args, **kwargs):
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email'])
+        email_verifications = EmailVerification.objects.filter(user=user, code=code)
+        if email_verifications.exists() and not email_verifications.first().is_expired():
+            user.is_verified_email = True
+            user.save()
+            return super(EmailVerificationView, self).get(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('index'))
